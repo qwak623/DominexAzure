9b3af1ba8a78338c0cb5501478d2d7c8da8123bf
Jan Babušík <qwak623@gmail.com>
2025-07-27 17:56:29 +0200

    market test

---
+++ GameCoreTests/Cards/Base/MarketTests.cs
@@ -0,0 +1,37 @@
+﻿using GameCore.Cards.Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace GameCore.Cards.Base.Tests;
+
+[TestClass]
+public class MarketTests : CardTestsBase
+{
+	private readonly Card market = Market.Get();
+
+	private Mock<IPlayer> player;
+
+	[TestInitialize]
+	public void Init()
+	{
+		player = MockPlayer(MockKingdom(market));
+	}
+
+	[TestMethod]
+	public void Play()
+	{
+		#region act
+		market.WhenPlayAction(player.Object);
+		#endregion
+
+		#region assert
+		// +1 Action, +1 Coin, +1 Buy
+		Assert.AreEqual(1, player.Object.PlayerState.Actions);
+		Assert.AreEqual(1, player.Object.PlayerState.Coins);
+		Assert.AreEqual(1, player.Object.PlayerState.Buys);
+
+		// player draws one card
+		player.Verify(p => p.Draw(1), Times.Once);
+		#endregion
+	}
+}
