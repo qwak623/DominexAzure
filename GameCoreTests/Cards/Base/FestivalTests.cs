4f68c406ecbb08bcd9ab4cc4cdfb8028d448514a
Jan Babušík <qwak623@gmail.com>
2025-07-27 16:13:33 +0200

    festival tests

---
+++ GameCoreTests/Cards/Base/FestivalTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GameCore.Cards.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameCore.Cards.Tests;
+using Moq;
+using GameCore.Cards.GeneralCards;
+
+namespace GameCore.Cards.Base.Tests;
+
+[TestClass]
+public class FestivalTests : CardTestsBase
+{
+	private readonly Card festival = Festival.Get();
+
+	private Mock<IPlayer> player;
+
+	[TestInitialize]
+	public void Init()
+	{
+		player = MockPlayer(MockKingdom(festival));
+	}
+
+	[TestMethod]
+	public void Play()
+	{
+		#region act
+		festival.WhenPlayAction(player.Object);
+		#endregion
+
+		#region assert
+		// actions, coins and buys shouldn't change 
+		Assert.AreEqual(2, player.Object.PlayerState.Actions);
+		Assert.AreEqual(2, player.Object.PlayerState.Coins);
+		Assert.AreEqual(1, player.Object.PlayerState.Buys);
+
+		// player does not draw a card
+		player.Verify(p => p.Draw(It.IsAny<int>()), Times.Never);
+		#endregion
+	}
+}
