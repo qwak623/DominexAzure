0313e002914361ccf304396308583a07734f4a91
Jan Babušík <qwak623@gmail.com>
2025-07-28 14:45:41 +0200

    player mine tests

---
+++ GameCoreTests/CardsWithPlayer/Base/PlayerMineTests.cs
@@ -0,0 +1,141 @@
+﻿using GameCore.Cards;
+using GameCore.Cards.Base;
+using GameCore.Cards.GeneralCards;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace GameCore.CardsWithPlayer.Base.Tests;
+
+[TestClass]
+public class PlayerMineTests
+{
+	private readonly Card mine = Mine.Get();
+	private readonly Card copper = Copper.Get();
+	private readonly Card silver = Silver.Get();
+
+	private Player player;
+
+	private Mock<IUser> user;
+	private Mock<IGame> game;
+
+	[TestInitialize]
+	public void Init()
+	{
+		var kingdom = new Kingdom(new() { mine }, 2);
+
+		game = new Mock<IGame>();
+		game.Setup(g => g.Kingdom).Returns(kingdom);
+		game.Setup(g => g.Trash).Returns(new List<Card> { });
+
+		user = new Mock<IUser>();
+
+		player = new Player(game.Object, user.Object);
+		player.PlayerState.Actions = 1;
+		player.PlayerState.Buys = 0;
+		player.PlayerState.Coins = 0;
+		player.PlayerState.PlayedCards = new List<Card> { };
+	}
+
+	[TestMethod]
+	public void UpgradeCopperToSilver()
+	{
+		#region arrange
+		user.Setup(u => u.MineTrash(It.IsAny<Card>(), It.IsAny<PlayerState>(), It.IsAny<Kingdom>(), It.IsAny<IList<Card>>()))
+			.Returns(copper);
+		user.Setup(u => u.SelectCardToGain(It.IsAny<KingdomWrapper>(), It.IsAny<PlayerState>(), It.IsAny<Kingdom>(), Phase.Gain))
+			.Returns(silver);
+
+		player.PlayerState.Hand = new List<Card> { mine, copper };
+		#endregion
+
+		#region act
+		player.PlayActionCardInternal(mine);
+		#endregion
+
+		#region assert
+		// -1 Action
+		Assert.AreEqual(0, player.PlayerState.Actions);
+
+		// coins and buys shouldn't change
+		Assert.AreEqual(0, player.PlayerState.Coins);
+		Assert.AreEqual(0, player.PlayerState.Buys);
+
+		// copper was trashed
+		CollectionAssert.AreEqual(new List<Card> { copper }, player.Game.Trash.ToList());
+
+		// silver was gained to hand
+		CollectionAssert.AreEqual(new List<Card> { silver }, player.PlayerState.Hand);
+
+		// mine was added to played cards
+		CollectionAssert.AreEqual(new List<Card> { mine }, player.PlayerState.PlayedCards);
+		#endregion
+	}
+
+	[TestMethod]
+	public void DontTrashAnything()
+	{
+		#region arrange
+		user.Setup(u => u.MineTrash(It.IsAny<Card>(), It.IsAny<PlayerState>(), It.IsAny<Kingdom>(), It.IsAny<IList<Card>>()))
+			.Returns<Card>(null);
+
+		player.PlayerState.Hand = new List<Card> { mine, copper };
+		#endregion
+
+		#region act
+		player.PlayActionCardInternal(mine);
+		#endregion
+
+		#region assert
+		// -1 Action
+		Assert.AreEqual(0, player.PlayerState.Actions);
+
+		// coins and buys shouldn't change
+		Assert.AreEqual(0, player.PlayerState.Coins);
+		Assert.AreEqual(0, player.PlayerState.Buys);
+
+		// nothing was trashed
+		Assert.IsFalse(player.Game.Trash.Any());
+
+		// copper stayed in hand
+		CollectionAssert.AreEqual(new List<Card> { copper }, player.PlayerState.Hand);
+
+		// mine was added to played cards
+		CollectionAssert.AreEqual(new List<Card> { mine }, player.PlayerState.PlayedCards);
+		#endregion
+	}
+
+	[TestMethod]
+	public void DontGainAnything()
+	{
+		#region arrange
+		user.Setup(u => u.MineTrash(It.IsAny<Card>(), It.IsAny<PlayerState>(), It.IsAny<Kingdom>(), It.IsAny<IList<Card>>()))
+			.Returns(copper);
+		user.Setup(u => u.SelectCardToGain(It.IsAny<KingdomWrapper>(), It.IsAny<PlayerState>(), It.IsAny<Kingdom>(), Phase.Gain))
+			.Returns<Card>(null);
+
+		player.PlayerState.Hand = new List<Card> { mine, copper };
+		#endregion
+
+		#region act
+		player.PlayActionCardInternal(mine);
+		#endregion
+
+		#region assert
+		// -1 Action
+		Assert.AreEqual(0, player.PlayerState.Actions);
+
+		// coins and buys shouldn't change
+		Assert.AreEqual(0, player.PlayerState.Coins);
+		Assert.AreEqual(0, player.PlayerState.Buys);
+
+		// copper was trashed
+		CollectionAssert.AreEqual(new List<Card> { copper }, player.Game.Trash.ToList());
+
+		// nothing was gained to hand
+		Assert.IsFalse(player.PlayerState.Hand.Any());
+
+		// mine was added to played cards
+		CollectionAssert.AreEqual(new List<Card> { mine }, player.PlayerState.PlayedCards);
+		#endregion
+	}
+}
