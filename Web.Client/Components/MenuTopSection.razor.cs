91027c0879ddca8cff03c2bfbaf391f97cbfd497
Jan Babušík <qwak623@gmail.com>
2025-08-27 14:18:25 +0200

    moved start button to its own component

---
+++ Web.Client/Components/MenuTopSection.razor.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Dominex.Web.Client.Components;
+
+public partial class MenuTopSection
+{
+	[Parameter] public Func<Task> ClickStart { get; set; }
+}
