799b7b35d686522ba87cdc7d603a281f3d391ae2
Jan Babušík <qwak623@gmail.com>
2025-08-28 10:40:29 +0200

    tools and settings menu

---
+++ Web.Client/Components/ToolsAndSettings.razor.cs
@@ -0,0 +1,46 @@
+﻿namespace Dominex.Web.Client.Components;
+
+public partial class ToolsAndSettings
+{
+	private bool showGenerators = false;
+	private bool showAISettings = false;
+	private bool showAdvancedSettings = false;
+
+	private List<string> PresetGames = new();
+	private List<string> CustomGames = new();
+
+	private void ClickGenerators()
+	{
+		showGenerators = !showGenerators;
+	}
+
+	private void ClickAI()
+	{
+		showAISettings = !showAISettings;
+	}
+
+	private void ClickAdvanced()
+	{
+		showAdvancedSettings = !showAdvancedSettings;
+	}
+
+	private void ClickRandom()
+	{
+
+	}
+
+	private void ClickAddRandom()
+	{
+
+	}
+
+	private void ClickCustomGame(string game)
+	{
+
+	}
+
+	private void ClickPresetGame(string game)
+	{
+
+	}
+}
