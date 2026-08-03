fd6a09320cd85757a0cf68873ae5de2fb9257bb5
Jan Babušík <qwak623@gmail.com>
2026-08-03 09:52:03 +0200

    intrique Harem/Farm

---
+++ GameCore/Cards/Intrique/Harem.cs
@@ -0,0 +1,22 @@
+﻿namespace GameCore.Cards.Intrique;
+public class Harem : Card
+{
+	// TODO Use 8 Harems/Farms for games with 2 players, 12 for games with 3 or more players.
+	private static Harem harem;
+	private Harem() : base
+	(
+		name: "Harem",
+		type: CardType.Harem,
+		price: 0,
+		addBuys: 0,
+		coins: 2,
+		isVictory: true,
+		isTreasure: true
+	)
+	{
+		harem = this;
+		VictoryPoints = 2;
+	}
+
+	public static Harem Get() => harem ?? new Harem();
+}
